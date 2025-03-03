893d27516496286035f89b1c04f245309d05597c
subham-bhadani <shubham.nwd28@gmail.com>
2025-03-03 16:01:31 +0530

    added api's in swagger.json

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 var corsOptions = {
   origin: "http://localhost:3001",
-  // optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  // optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
